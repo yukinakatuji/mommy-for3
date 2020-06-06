a917cf3823be621abec181f751f645498d33c9e0
yukinakatuji <sizuku1216drop@yahoo.co.jp>
2020-06-06 23:57:58 +0900

    flont create1

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,6 @@
+class MenusController < ApplicationController
+  def index
+  #   @menus = 
+  #   @menus = @messages.includes(:user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  # sessions: 'users/sessions',
+  # registrations: 'users/registrations'
+  root to: 'menus#index'
+  # root to: 'mypage#index'
+
+  # resources :users, only: [:index, :edit, :update]
+  # resources :mypage, only: [:index, :show, :new, :create, :edit, :update] 
+  # resources :relationships, only: [:create, :destroy]
+
 end
