91373d183dc02b24f9a8aa5c0fc37c76f752e80a
yukinakatuji <sizuku1216drop@yahoo.co.jp>
2020-06-10 11:54:42 +0900

    send-messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ApplicationRecord
 
   validates :content, presence: true, unless: :image?
 
-#   mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_many :group_users
   has_many :groups, through: :group_users
-  
+  has_many :messages
+   
   validates :name, presence: true, uniqueness: true
 
 end
